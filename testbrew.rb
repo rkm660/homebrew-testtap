b23167e668cea7198fc29f90f3ec8a7e5eb09577
Rahul Matta <rahulmatta2016@u.northwestern.edu>
2020-05-13 16:45:29 -0500

    add formula

---
+++ testbrew.rb
@@ -0,0 +1,11 @@
+class Testbrew < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/rkm660/testbrew/archive/v1.0.0.tar.gz"
+  sha256 "89c653807fa0ce59ef08aaa5a319f0a086cecc182f1a6e63189657b37133c577"
+
+  def install
+	bin.install "testbrew"
+  end
+
+end
